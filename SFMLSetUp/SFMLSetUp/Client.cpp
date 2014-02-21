6c899614b7ed515ab4dadc9ca6a57be425c213f0
Handerson <handersl@uci.edu>
2014-02-21 14:04:05 -0800

    Added in the base Client and Server class.

---
+++ SFMLSetUp/SFMLSetUp/Client.cpp
@@ -0,0 +1,95 @@
+#include "Client.h"
+
+Client::Client()
+{
+	connected = false;
+}
+
+Client::Client(const std::string& ipAddress, const int& port)
+{
+	socket = new sf::TcpSocket;
+	sf::Socket::Status status = socket->connect(ipAddress, port);
+	selector.add(*socket);
+	
+	connected = true;
+
+	//Create the thread to always be checking for packets.
+	clientThread = new sf::Thread(&Client::receive, this);
+	clientThread->launch();
+}
+
+Client::~Client()
+{
+	//Saftly end the thread and delete it.
+	connected = false;
+	clientThread->terminate();
+	delete clientThread;
+	delete socket;
+}
+
+void Client::connect(const std::string& ipAddress, const int& port)
+{
+	//Connect should only be called when creating a client using the
+	//default constructor.  it does the same thing as connected
+	//with arguments.
+	if(!connected)
+	{
+		sf::Socket::Status status = socket->connect(ipAddress, port);
+		selector.add(*socket);
+
+		connected = true;
+
+		//Create the thread to always be checking for packets.
+		clientThread = new sf::Thread(&Client::receive, this);
+		clientThread->launch();
+	}
+}
+
+void Client::send(const std::string& stringToSend)
+{
+	sf::Packet stringPacket;
+	stringPacket << stringToSend;
+	
+	if(socket->send(stringPacket) != sf::Socket::Done)
+	{
+		//ERROR
+	}
+}
+
+void Client::receive()
+{
+	while(connected)
+	{
+		//If this is true then there is something to be read.
+		if(selector.wait())
+		{
+			//Create the packet to recieve the incoming one.
+			sf::Packet receivePacket;
+			if(socket->receive(receivePacket) != sf::Socket::Done)
+			{
+				//ERROR
+			}
+			
+			//Lock the queue.
+			sf::Lock lock(*mutex);
+
+			//Add to queue.
+			clientPacketQueue.push(receivePacket);
+		}
+		//Mutex unlocks here.
+	}
+}
+
+sf::Packet Client::getTopPacket()
+{
+	sf::Packet packetToReturn;
+
+	if(!clientPacketQueue.empty())
+	{
+		sf::Lock lock(*mutex);
+		packetToReturn = clientPacketQueue.front();
+		clientPacketQueue.pop();
+	}//Mutex unlocks here.
+
+	return packetToReturn;
+}
